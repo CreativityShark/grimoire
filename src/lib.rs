229e42bd19eee0637f08d81d6539a518675ddb33
Creativity Shark <94210249+CreativityShark@users.noreply.github.com>
2024-07-16 21:17:42 -0500

    Add command functionality (but good now)

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,137 @@
-pub fn run() {
-    println!("epic!");
+use std::error::Error;
+
+pub fn run(command: Box<dyn Command>) -> Result<(), Box<dyn Error>> {
+    command.run()?;
+
+    Ok(())
+}
+
+pub fn build_command(mut args: impl Iterator<Item = String>) -> Result<Box<dyn Command>, &'static str> {
+    // First argument is program itself, skip with next()
+    args.next();
+
+    let cmd = match args.next() {
+        Some(cmd) => cmd,
+        None => return Err("Missing command!"),
+    };
+
+    if &cmd[..] == "add" {
+        Ok(TestAdd::build(&mut args)?)
+    } else if &cmd[..] == "say" {
+        Ok(TestSay::build(&mut args)?)
+    } else {
+        Err("Invalid command!")
+    }
+}
+
+pub trait Command {
+    fn build(args: &mut impl Iterator<Item = String>) -> Result<Box<Self>, &'static str> where Self: Sized;
+    fn run(&self) -> Result<(), Box<dyn Error>>;
 }
 
 #[derive(Debug)]
-pub enum CommandKind {
-    TestSay(String),
-    TestAdd(Vec<i32>),
+pub struct TestAdd {
+    nums: Vec<i32>,
+}
+
+impl Command for TestAdd {
+    fn build(args: &mut impl Iterator<Item = String>) -> Result<Box<Self>, &'static str> where Self: Sized {
+        let nums: Vec<i32> = args 
+            .filter_map(|n| {
+                match n.parse::<i32>() {
+                    Ok(x) => Some(x),
+                    _ => None,
+                   }
+            }).collect();
+
+        if nums.len() <= 0 { return Err("Missing number(s) to add!"); }
+
+        Ok(Box::new(TestAdd { nums }))
+    }
+
+    fn run(&self) -> Result<(), Box<dyn Error>> {
+        let sum: i32 = self.nums
+            .iter()
+            .sum();
+        println!("{}", sum);
+
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
-pub struct Command {
-    pub kind: CommandKind,
+pub struct TestSay {
+    message: String,
 }
 
-impl Command {
-    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Command, &'static str> {
-        // First item is always name of program, use next() to get rid of it
-        args.next();
-
-        let kind = match args.next() {
-            Some(cmd) => match Self::get_command_type(&cmd, args) {
-                Ok(kind) => kind,
-                Err(e) => return Err(e),
-            },
-            None => return Err("Missing a command!"),
+impl Command for TestSay {
+    fn build(args: &mut impl Iterator<Item = String>) -> Result<Box<Self>, &'static str> where Self: Sized {
+        let message = match args.next() {
+            Some(s) => String::from(s),
+            None => return Err("Missing message to print!"),
         };
 
-        Ok(Command { kind })
+        Ok(Box::new(TestSay { message }))
     }
 
-    fn get_command_type(cmd: &str, mut args: impl Iterator<Item = String>) -> Result<CommandKind, &'static str> {
-        match cmd {
-            "add" => {
-                let nums: Vec<i32> = args 
-                    .filter_map(|n| {
-                        match n.parse::<i32>() {
-                            Ok(x) => Some(x),
-                            _ => None,
-                        }
-                    }).collect();
+    fn run(&self) -> Result<(), Box<dyn Error>> {
+        println!("{}", self.message);
 
-                if nums.len() <= 0 { return Err("Missing number(s) to add!"); }
-
-                Ok(CommandKind::TestAdd(nums))
-            },
-            "say" => {
-                let message = match args.next() {
-                    Some(s) => String::from(s),
-                    None => return Err("Missing a message!"),
-                };
-
-                Ok(CommandKind::TestSay(message))
-            },
-            _ => return Err("Invalid command!"),
-        }
+        Ok(())
     }
 }
+
+//#[derive(Debug)]
+//pub enum CommandKind {
+//    TestSay(String),
+//    TestAdd(Vec<i32>),
+//}
+//
+//#[derive(Debug)]
+//pub struct Command {
+//    pub kind: CommandKind,
+//}
+//
+//impl Command {
+//    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Command, &'static str> {
+//        // First item is always name of program, use next() to get rid of it
+//        args.next();
+//
+//        let kind = match args.next() {
+//            Some(cmd) => match Self::get_command_type(&cmd, args) {
+//                Ok(kind) => kind,
+//                Err(e) => return Err(e),
+//            },
+//            None => return Err("Missing a command!"),
+//        };
+//
+//        Ok(Command { kind })
+//    }
+//
+//    fn get_command_type(cmd: &str, mut args: impl Iterator<Item = String>) -> Result<CommandKind, &'static str> {
+//        match cmd {
+//            "add" => {
+//                let nums: Vec<i32> = args 
+//                    .filter_map(|n| {
+//                        match n.parse::<i32>() {
+//                            Ok(x) => Some(x),
+//                            _ => None,
+//                        }
+//                    }).collect();
+//
+//                if nums.len() <= 0 { return Err("Missing number(s) to add!"); }
+//
+//                Ok(CommandKind::TestAdd(nums))
+//            },
+//            "say" => {
+//                let message = match args.next() {
+//                    Some(s) => String::from(s),
+//                    None => return Err("Missing a message!"),
+//                };
+//
+//                Ok(CommandKind::TestSay(message))
+//            },
+//            _ => return Err("Invalid command!"),
+//        }
+//    }
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::env;
 use std::process;
 
-use grimoire::Command;
-
 fn main() {
-    println!("Hello, world!");
-
-    let command = Command::build(env::args()).unwrap_or_else(|e| {
-        eprintln!("Error! {e}");
+    let cmd = grimoire::build_command(env::args()).unwrap_or_else(|e| {
+        eprintln!("Error: {e}");
         process::exit(1);
     });
 
-    dbg!("{:?}", command.kind);
+    if let Err(e) = grimoire::run(cmd) {
+        eprintln!("Application Error: {e}");
+        process::exit(1);
+    }
 }
